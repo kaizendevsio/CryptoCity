8571f1bcd0d7be135d8a8df1803db3760a369f58
Jeeeeeyo <47585152+Jeeeeeyo@users.noreply.github.com>
2019-12-20 20:53:37 +0800

    UI

---
+++ CryptoCityWallet.FrontEnd/Models/AdminVM.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoCityWallet.FrontEnd.Models
+{
+    public class AdminVM
+    {
+        public string Fullname { get; set; }
+        public string Username { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
