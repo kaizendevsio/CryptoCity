8c45f60f2db64898d7645677b4c857401b1ffc29
kaizendevsio <jjeeraldo@gmail.com>
2019-12-22 17:18:00 +0800

    Mail Service

---
+++ CryptoCityWallet.AppService/MailAppService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace CryptoCityWallet.AppService
+{
+   public class MailAppService
+    {
+        public async Task<bool> SendAsync()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("test@example.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+
+            return true;
+        }
+    }
+}
